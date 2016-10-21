627e338c2147a3f02cb106c0edd5fe961977013b
Kyle Juthan <kylejuthan@hotmail.com>
2016-10-21 12:55:32 +0100

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+      System.out.println("Hello Kyle");
+	  
+    }
+    
+}
